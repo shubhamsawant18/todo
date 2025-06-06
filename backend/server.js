575eb9e545a56d9172d62f3031f2f17ab344dcc9
shubhamsawant18 <shubhsawant124@gmail.com>
2025-06-06 19:48:15 +0530

    all backend api are working

--- backend/server.js
+++ backend/server.js
@@ -2,23 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const taskRoutes = require('./routes/taskRoutes');
 
-dotenv.config(); // Load environment variables
+dotenv.config();
 
 const app = express();
-
-// Middleware
 app.use(express.json());
 app.use(cors());
 
-// Connect to database
-connectDB();
+connectDB(); // Connect Database
 
-// Basic route to test server
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
+// Use Routes
+app.use('/api/tasks', taskRoutes);
 
-// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
